95db3ed7046fe75e5f5fd322eaf6125fe387a595
geppel <d.y.ganso.g@gmail.com>
2020-04-16 08:28:29 +0900

    階乗関数の実装

--- app.js
+++ app.js
@@ -7,7 +7,14 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    /*
+    for (let i = 2; i <= n; i++) {
+        result = result * i;
+    }
+    */
+    for (let i = n; i >= 2; i--) {
+        result = result * i;
+    }
     return result;
 }
 const assert = require('assert');
